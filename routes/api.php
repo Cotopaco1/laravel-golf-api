1afe9ea606e2de37bb5520b50a322e159051aeef
sergio silva <cotopaco123@hotmail.es>
2026-01-10 17:52:51 -0500

    refactor(listing-routes): Register routes manually and make public listings.index route

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,11 @@ use App\Http\Controllers\v1\AuthenticationController;
 Route::prefix('v1')->group(function(){
 
     Route::middleware('auth:sanctum')->group(function(){
-        Route::apiResource('listings', ListingController::class);
+//        Route::apiResource('listings', ListingController::class);
+        Route::post('listings', [ListingController::class, 'store'])->name('listings.store');
+        Route::delete('listings/{listing}', [ListingController::class, 'destroy'])->name('listings.destroy');
     });
+    Route::get('listings', [ListingController::class, 'index'])->name('listings.index');
 
     Route::middleware(['guest', 'throttle:login'])->group(function(){
        Route::post('login', [AuthenticationController::class, 'login'])->name('login');
